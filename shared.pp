949ea53482d0810ec1acfc6cb467f7fd8688910c
Nir Boneh <nboneh5921@gmail.com>
2016-10-09 19:24:42 -0600

    Added shared script

--- shared.pp
+++ shared.pp
@@ -7,3 +7,23 @@ file { '/home/sales':
 exec{'sales_inherit':
 	command => "/bin/chmod g+s /home/sales",
 }
+
+file { '/home/managers':
+	ensure => 'directory',
+	owner => 'root',
+	group => 'managers',
+	mode => '0070',
+}
+exec{'managers_inherit':
+	command => "/bin/chmod g+s /home/managers",
+}
+
+file { '/home/accounting':
+	ensure => 'directory',
+	owner => 'root',
+	group => 'accounting',
+	mode => '0070',
+}
+exec{'accounting_inherit':
+	command => "/bin/chmod g+s /home/accounting",
+}
