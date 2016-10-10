a51b19100c867e3e8d90bf9bf2d8f00e39bc9915
Nir Boneh <nboneh5921@gmail.com>
2016-10-09 18:24:10 -0600

    Updating crap

--- lab.pp
+++ lab.pp
@@ -42,9 +42,78 @@ useradd{ 'mscott':
 	secgroup => 'managers',
 }
 
-useradd{ 'nboneh2':
+useradd{ 'jhalpert':
 	uuid => 105,
 	secgroup => 'managers',
 }
 
+useradd{ 'dschrute':
+	uuid => 106,
+	secgroup => 'managers',
+}
+
+useradd{ 'kmalone':
+	uuid => 107,
+	secgroup => 'accounting',
+}
+
+useradd{ 'omartinez':
+	uuid => 108,
+	secgroup => 'accounting',
+}
+
+useradd{ 'amartin':
+	uuid => 109,
+	secgroup => 'accounting',
+}
+
+useradd{ 'shudson':
+	uuid => 110,
+	secgroup => 'sales',
+}
+
+useradd{ 'plapin':
+	uuid => 111,
+	secgroup => 'sales',
+}
+
+useradd{ 'abernard':
+	uuid => 112,
+	secgroup => 'sales',
+}
+
+useradd{ 'pbeesly':
+	uuid => 113,
+	secgroup => 'sales',
+}
+
+useradd{ 'kkapoor':
+	uuid => 114,
+	secgroup => 'sales',
+}
+
+useradd{ 'mpalmer':
+	uuid => 115,
+	secgroup => 'sales',
+}
+
+useradd{ 'tflenderson':
+	uuid => 116,
+	secgroup => 'sales',
+}
+
+useradd{ 'cbratton':
+	uuid => 117,
+	secgroup => 'sales',
+}
+
+useradd{ 'dphilbin':
+	uuid => 118,
+	secgroup => 'sales',
+}
+
+useradd{ 'nboneh':
+	uuid => 119,
+	secgroup => 'managers',
+}
 
